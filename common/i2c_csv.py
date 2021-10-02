e6135157f540cef29ca3e6599b98f6bb64f2369d
Addison Hammer <addison.hammer@gmail.com>
2021-10-02 07:54:43 -0700

    Start a 'common' module for common libraries

---
+++ common/i2c_csv.py
@@ -0,0 +1,31 @@
+import time
+import datetime
+from typing import Type
+import smbus
+import csv
+import logging
+
+BUS = smbus.SMBus(1)
+
+def log_i2c_to_csv(read_interval_s: datetime.timedelta,
+                    read_duration_s: datetime.timedelta,
+                    csv_file: str,  
+                    data_header: str,
+                    data_type: Type,
+                    i2c_addr: bytes = 0x40,
+                    i2c_offset: bytes = 0x00,
+                    i2c_len: int = 8):
+    with open(csv_file, 'w') as f:
+            csv_writer = csv.DictWriter(f, ('timestamp', data_header))
+            start_time = datetime.datetime.utcnow()
+            while (datetime.datetime.utcnow() - start_time) < read_duration_s:
+                try:
+                    result = BUS.read_i2c_block_data(i2c_addr, i2c_offset, i2c_len)
+                    msg = bytes(b for b in result if b != 255).decode('utf-8')
+                    logging.info(f'Logging temperature reading: {msg}')
+                    timestamp = datetime.datetime.utcnow()
+                    csv_writer.writerow({'timestamp': timestamp.isoformat(), data_header: data_type(msg)})
+                    time.sleep(read_interval_s.total_seconds())
+                except OSError as e:
+                    logging.exception('IO Error on I2C Bus:')
+                    continue
