46ee1f4c11aa657a5d16b866c2b12c2c38a5ca47
addisonhammer <60857822+addisonhammer@users.noreply.github.com>
2021-10-24 09:04:36 -0700

    ThingSpeak Bugfix, Prettier Display, RR Arch
    - Added debug/error printing and response output for ThingSpeak HTTP Requests
    - Fixed bug preventing temperature readings from printing properly
    - Draw OLED Temp and Humidity prettier, with header
    - Add OLED "Updating ThingSpeak" indicator
    - Limit DHT Update rate to 1Hz (per documentation)
    - Refactor main loop to RR Architecture - timer-based updates for DHT, OLED, ThingSpeak

--- Project_5/project_5.ino
+++ Project_5/project_5.ino
@@ -1,3 +1,5 @@
+#include <millisDelay.h>
+
 #include <OLEDDisplay.h>
 #include <OLEDDisplayFonts.h>
 #include <OLEDDisplayUi.h>
@@ -9,19 +11,24 @@
 #include <ESP8266HTTPClient.h>
 
 // WIFI Auth
-const char WIFI_NAME[] = "SeriesOfTubesGuest";
-const char WIFI_PASS[] = "19386592";
+const char WIFI_NAME[] = "{your SSID}";
+const char WIFI_PASS[] = "{your password}";
 
 // Display Variable
 SSD1306Wire display(0x3c, D3, D4);
+
 // Temperature and Humidity Sensor
 DHTesp dht;
+const int DHT_UPDATE_PERIOD = 1000;
+millisDelay DHTreadDelay;
 
 const int PRINT_BUFF_SIZE = 80;
-char printbuff[PRINT_BUFF_SIZE];
+char FormattedTemperature[PRINT_BUFF_SIZE];
+char FormattedHumidity[PRINT_BUFF_SIZE];
 
 const int FRAME_RATE = 60;
 const int FRAME_PERIOD = 1000 / FRAME_RATE;
+millisDelay OLEDframeDelay;
 
 const char TSPEAK_WRITE_KEY[] = "S687MH9LQ0GGPPPC";
 const char TSPEAK_WRITE_URL_FMT[] = "http://api.thingspeak.com/update?api_key=%s&field1=%f&field2=%f";
@@ -29,54 +36,43 @@ const int URLBUF_SIZE = sizeof(TSPEAK_WRITE_URL_FMT) + 80;
 char urlbuf[URLBUF_SIZE];
 const char TSPEAK[] = "https://api.thingspeak.com/  ";
 unsigned long TSPEAK_INTERVAL_MS = 20000;
-
-unsigned long timeOfLastUpdate = 0;
+millisDelay thingSpeakDelay;
+millisDelay thingSpeakInidicatorDelay;
+bool tspeakIsUpdating = false;
 
 const int NUM_FIELDS = 2;
 int currentFieldNum = 1;
 
+float temperature = 0.0;
+float humidity = 0.0;
 
-void writeTSpeakField(int field1Val, float field2Val)
-{
-    if (WiFi.status() == WL_CONNECTED) 
-    { //Check WiFi connection status
-
-      WiFiClient client;
-      HTTPClient http;  //Declare an object of class HTTPClient
-      
-      snprintf(urlbuf, URLBUF_SIZE, TSPEAK_WRITE_URL_FMT, TSPEAK_WRITE_KEY, field1Val, field2Val);
-
-      http.begin(client, urlbuf);  //Specify request destination
-      int httpCode = http.GET();                                  //Send the request
-   
-      if (httpCode > 0) { //Check the returning code
-   
-        String payload = http.getString();   //Get the request response payload
-        Serial.println(payload);             //Print the response payload
-   
-      }
-      
-      http.end();   //Close connection
+void writeTSpeakField(float field1Val, float field2Val) {
+  WiFiClient client;
+  HTTPClient http;  // Declare an object of class HTTPClient
+  snprintf(urlbuf, URLBUF_SIZE, TSPEAK_WRITE_URL_FMT, TSPEAK_WRITE_KEY, field1Val, field2Val);
+  http.begin(client, urlbuf);  // Specify request destination
+  Serial.print("ThingSpeak request: ");
+  Serial.println(urlbuf);  // Print the request payload
+  int httpCode = http.GET();  // Send the request
+  if (httpCode != HTTP_CODE_OK) { // Check the returning code
+    Serial.print("ThingSpeak HTTP Error: ");
+    Serial.print(httpCode);
+    Serial.print(" ");
+    Serial.println(http.errorToString(httpCode));
   }
-  else
-  {
-    Serial.println("Wifi Not Connected");
+  else {
+  String payload = http.getString();  // Get the request response payload
+  Serial.print("Success! New Record ID: ");
+  Serial.println(payload); // Print the response payload
   }
+http.end();  // Close connection
 }
 
-
-void tspeakUpdate(float tempC, float humidP)
-{
-  writeTSpeakField(1, tempC);
-  writeTSpeakField(2, humidP);
-}
-
-
-void setup() 
+void setup()
 {
   // Setup debugging prints
   Serial.begin(115200);
-  
+
   // Setup Display
   display.init();
   display.clear();
@@ -88,6 +84,7 @@ void setup()
   dht.setup(D6, DHTesp::DHT11);
 
   // Setup Wifi
+  Serial.println("Connecting to WiFi.");
   WiFi.begin(WIFI_NAME, WIFI_PASS);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
@@ -97,27 +94,63 @@ void setup()
   Serial.println("WiFi connected");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
+
+  // Start Timers
+  DHTreadDelay.start(DHT_UPDATE_PERIOD);
+  OLEDframeDelay.start(FRAME_PERIOD);
+  thingSpeakDelay.start(TSPEAK_INTERVAL_MS);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  display.clear();
-  float temperature = dht.getTemperature();
-  float humidity = dht.getHumidity();
-
-
-  snprintf(printbuff, PRINT_BUFF_SIZE, "Temp C: %f\n Humidity %: %f", temperature, humidity);
-  display.drawString(0, 0, printbuff);
-
-  unsigned long currentTime = millis();
-  if(currentTime + TSPEAK_INTERVAL_MS > timeOfLastUpdate){
-    timeOfLastUpdate = currentTime;
-    Serial.println("Updating Server...");
-    tspeakUpdate(temperature, humidity);
+  if (DHTreadDelay.justFinished()) {
+    update_temp();
   }
-  
+  if (OLEDframeDelay.justFinished()) {
+    OLEDframeDelay.repeat();
+    update_oled();
+  }
+  if (thingSpeakDelay.justFinished()) {
+    thingSpeakDelay.repeat();
+    thingSpeakInidicatorDelay.start(1000);
+    tspeakIsUpdating = true;
+    if (WiFi.status() != WL_CONNECTED) { //Check WiFi connection status
+      Serial.println("Wifi Not Connected");
+      return;
+    }
+    writeTSpeakField(temperature, humidity);
+  }
+  if (thingSpeakInidicatorDelay.justFinished()) {
+    tspeakIsUpdating = false;
+  }
+}
+
+void update_temp() {
+  temperature = dht.toFahrenheit(dht.getTemperature());
+  humidity = dht.getHumidity();
+}
+
+void update_oled() {
+  display.clear();
+  drawIndoor(&display, temperature, humidity);
+  if (tspeakIsUpdating) {
+    drawUpdating(&display);
+  }
   display.display();
+}
 
-  delay(FRAME_PERIOD);
+void drawIndoor(SSD1306Wire * display, float temperature, float humidity) {
+  display->setTextAlignment(TEXT_ALIGN_CENTER);
+  display->setFont(ArialMT_Plain_10);
+  display->drawString(64, 0, "DHT22 Indoor Sensor");
+  display->setFont(ArialMT_Plain_16);
+  dtostrf(temperature, 4, 1, FormattedTemperature);
+  display->drawString(64, 12, "Temp: " + String(FormattedTemperature) + ("°F"));
+  dtostrf(humidity, 4, 1, FormattedHumidity);
+  display->drawString(64, 30, "Humidity: " + String(FormattedHumidity) + "%");
+}
 
+void drawUpdating(SSD1306Wire * display) {
+  display->setTextAlignment(TEXT_ALIGN_CENTER);
+  display->setFont(ArialMT_Plain_10);
+  display->drawString(64, 48, "Updating Thingspeak...");
 }
