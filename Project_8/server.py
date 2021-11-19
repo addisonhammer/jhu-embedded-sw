3f31260b97d682471ee84c9ebbdd0b97ca467cc8
Addison Hammer <addison.hammer@gmail.com>
2021-11-19 12:11:25 -0800

    Use Turbo instead of WebSockets

--- Project_8/server.py
+++ Project_8/server.py
@@ -1,18 +1,38 @@
-from gevent import monkey
-monkey.patch_all()
-from time import sleep
-from flask import Flask, render_template, Response, render_template_string, session, copy_current_request_context
-import imu
+import random
+import re
 import sys
-from concurrent.futures import ThreadPoolExecutor, Future
-from flask_socketio import SocketIO, emit, disconnect
-from threading import Lock
+import imu
+from flask import Flask, render_template
+from turbo_flask import Turbo
+import threading
+import time
 
-
-
-arduino = None
 app = Flask(__name__)
-socket_ = SocketIO(app)
+turbo = Turbo(app)
+arduino = None
+
+def update_load():
+    with app.app_context():
+        while True:
+            time.sleep(0.5)
+            turbo.push(turbo.replace(render_template('imu.html.jinja'), 'imu'))
+
+@app.route('/')
+def index():
+    return render_template('index.html.jinja')
+
+@app.context_processor
+def inject_load():
+    global arduino
+    try:
+        data = arduino.read_data()
+    except:
+        return {'yaw': 0, 'pitch': 0, 'roll': 0}
+    return data.__dict__
+
+@app.before_first_request
+def before_first_request():
+    threading.Thread(target=update_load).start()
 
 @app.route("/gps")
 def get_gps():
@@ -27,33 +47,10 @@ def get_imu():
 def get_video():
     return "<p>Under Construction :(</p>"
 
-@app.route('/')
-def index():
-    return render_template('index.html', async_mode=socket_.async_mode)
-
-
-@socket_.on('imu_request', namespace='/test')
-def test_message():
-    session['stop'] = False
-    while not session['stop']:
-        emit('response', {'data': get_imu()})
-        sleep(0.2)
-
-@socket_.on('disconnect_request', namespace='/test')
-def disconnect_request():
-    @copy_current_request_context
-    def can_disconnect():
-        disconnect()
-
-    session['stop'] = True
-    emit('response',
-         {'data': 'Disconnected!'},
-         callback=can_disconnect)
-
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         port = 'COM4'
     else:
         port = sys.argv[1]
     arduino = imu.Arduino(port=port, baudrate=115200, timeout=0.1)
-    socket_.run(app, host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
