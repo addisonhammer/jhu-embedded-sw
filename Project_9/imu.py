620840fc3be1c9ebf395087c0fa21bd2c9ca7dca
Addison Hammer <addison.hammer@gmail.com>
2021-11-19 17:32:09 -0800

    Cleanup

--- Project_9/imu.py
+++ Project_9/imu.py
@@ -8,6 +8,7 @@ from serial.serialutil import SerialException
 _REQ = b'!'
 _DELIM = b','
 
+
 @dataclasses.dataclass(frozen=True, order=True)
 class ImuData:
     yaw: float
@@ -16,6 +17,7 @@ class ImuData:
 
     def to_json(self) -> str:
         return json.dumps(self.__dict__)
+
 
 class Arduino:
     def __init__(self, port, baudrate, timeout):
@@ -26,7 +28,8 @@ class Arduino:
 
     def read_data(self) -> ImuData:
         if not self.serial:
-          self.serial = serial.Serial(port=self.port, baudrate=self.baud, timeout=self.timeout)
+            self.serial = serial.Serial(
+                port=self.port, baudrate=self.baud, timeout=self.timeout)
         self.serial.write(_REQ)
         time.sleep(0.05)
         data = self.serial.readline().split(_DELIM)
@@ -36,25 +39,29 @@ class Arduino:
             return ImuData(*args)
         raise SerialException("Error Retrieving IMU Data!")
 
+    def read_json_bytes(self) -> bytes:
+        return self.read_data().to_json().encode('utf-8')
+
+
+
+def try_connect():
+    try:
+        return serial.Serial(port='/dev/rfcomm0', baudrate=115200, timeout=0.2)
+    except serial.serialutil.SerialException as e:
+        print('Bluetooth client not connected:')
+        print(e)
+        print('Retrying in 5s...')
+        time.sleep(5)
+        return None
 
 
 if __name__ == "__main__":
     a = Arduino(port='/dev/ttyUSB0', baudrate=115200, timeout=0.2)
-    client = None
     while True:
-        if client is None:
-            try:
-                client = serial.Serial(port='/dev/rfcomm0', baudrate=115200, timeout=0.2)
-                print('Connected! Streaming data...')
-            except serial.serialutil.SerialException as e:
-                print('Bluetooth client not connected:')
-                print(e)
-                print('Retrying in 5s...')
-                client = None
-                time.sleep(5)
-                continue
+        if not client:
+            client = try_connect()
         try:
-            client.write(a.read_data().to_json().encode('utf-8'))
+            client.write(a.read_json_bytes())
         except Exception as e:
             print(e)
             time.sleep(1)
