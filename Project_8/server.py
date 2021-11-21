b5d61e50454a75cd1ec565b212d9c4cbfb5a5543
Addison Hammer <addison.hammer@gmail.com>
2021-11-20 18:19:42 -0800

    Cleanup and consolidation

--- Project_8/server.py
+++ Project_8/server.py
@@ -1,24 +1,24 @@
-import random
-import re
 import sys
 import imu
 from flask import Flask, render_template
 from turbo_flask import Turbo
 import threading
-import time
 
 app = Flask(__name__)
 turbo = Turbo(app)
 arduino = None
+
+
+@app.before_first_request
+def before_first_request():
+    threading.Thread(target=update_load).start()
+
 
 def update_load():
     with app.app_context():
         while True:
             turbo.push(turbo.replace(render_template('imu.html.jinja'), 'imu'))
 
-@app.route('/')
-def index():
-    return render_template('index.html.jinja')
 
 @app.context_processor
 def inject_load():
@@ -29,22 +29,16 @@ def inject_load():
         return {'yaw': 0, 'pitch': 0, 'roll': 0}
     return data.__dict__
 
-@app.before_first_request
-def before_first_request():
-    threading.Thread(target=update_load).start()
 
-@app.route("/gps")
-def get_gps():
-    return "<p>Under Construction :(</p>"
+@app.route('/')
+def index():
+    return render_template('index.html.jinja')
+
 
 @app.route("/imu")
 def get_imu():
     global arduino
     return arduino.read_data().to_json()
-
-@app.route("/video")
-def get_video():
-    return "<p>Under Construction :(</p>"
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
